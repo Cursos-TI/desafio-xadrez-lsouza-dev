c1e7202f03124e584ff9f71298ca21cd70e54082
Luiz Fabiano de Souza <lsouza.dev04@gmail.com>
2026-06-27 22:30:09 -0300

    Nivel aventureiro

--- xadrez.c
+++ xadrez.c
@@ -9,11 +9,13 @@
 void menuBispo();
 void menuTorre();
 void menuRainha();
+void menuCavalo();
 
 void moverPeca(char pecaSelecionada);
 void movimentoBispo(char direcao, int quantidade);
 void movimentoTorre(char direcao, int quantidade);
 void movimentoRainha(char direcao, int quantidade);
+void movimentoCavalo(char cirecao);
 
 int main()
 {
@@ -36,6 +38,7 @@ int main()
     printf("\n[B] Bispo");
     printf("\n[T] Torre");
     printf("\n[R] Rainha");
+    printf("\n[C] Cavalo");
     printf("\n\nEscolha: ");
     scanf(" %s", &pecaSelecionada);
 
@@ -61,8 +64,10 @@ void moverPeca(char pecaSelecionada)
     char direcao;
     int qtdLimiteMovimentos;
 
-    if(pecaSelecionada == 'B' || pecaSelecionada == 'T') qtdLimiteMovimentos = 5;
-    else qtdLimiteMovimentos = 8;
+    if (pecaSelecionada == 'B' || pecaSelecionada == 'T')
+        qtdLimiteMovimentos = 5;
+    else
+        qtdLimiteMovimentos = 8;
 
     if (pecaSelecionada == 'B')
         menuBispo();
@@ -70,31 +75,41 @@ void moverPeca(char pecaSelecionada)
         menuTorre();
     if (pecaSelecionada == 'R')
         menuRainha();
+    if (pecaSelecionada == 'C')
+        menuCavalo();
 
     scanf("%s", &direcao);
 
     bool quantidadeValida = false;
-    do
+    if (pecaSelecionada != 'C')
     {
-
-        printf("Digite a quantidade de casas que deseja andar:");
-        scanf("%d", &qtdMovimentos);
-
-        quantidadeValida = qtdMovimentos > 0 && qtdMovimentos <= qtdLimiteMovimentos;
-
-        if (!quantidadeValida)
+        do
         {
-            printf("\nQuantidade de passos inválida\n");
-        }
+            printf("Digite a quantidade de casas que deseja andar:");
+            scanf("%d", &qtdMovimentos);
 
-    } while (!quantidadeValida);
+            quantidadeValida = qtdMovimentos > 0 && qtdMovimentos <= qtdLimiteMovimentos;
+
+            if (!quantidadeValida)
+            {
+                printf("\nQuantidade de passos inválida\n");
+            }
+
+        } while (!quantidadeValida);
+    }
 
     printf("\nIniciando o movimento\n");
 
-    if (pecaSelecionada == 'B') movimentoBispo(toupper(direcao),qtdMovimentos);
-    if (pecaSelecionada == 'T') movimentoTorre(toupper(direcao),qtdMovimentos);
-    if (pecaSelecionada == 'R') movimentoRainha(toupper(direcao),qtdMovimentos);
+    if (pecaSelecionada == 'B')
+        movimentoBispo(toupper(direcao), qtdMovimentos);
+    if (pecaSelecionada == 'T')
+        movimentoTorre(toupper(direcao), qtdMovimentos);
+    if (pecaSelecionada == 'R')
+        movimentoRainha(toupper(direcao), qtdMovimentos);
+    if (pecaSelecionada == 'C')
+        movimentoCavalo(toupper(direcao));
 
+        printf("\n\n");
 }
 
 void menuBispo()
@@ -116,11 +131,15 @@ void movimentoBispo(char direcao, int quantidade)
     bool isDown = direcao == 'C' || direcao == 'D';
     for (int i = 0; i < quantidade; i++)
     {
-        if(isUp)printf("\nCima");
-        if(isDown)printf("\nBaixo");
-        if(isLeft)printf("\nEsquerda");
-        if(isRight)printf("\nDireita");
-        printf("\nMovimentos restantes: %d \n",quantidade - (i+1));
+        if (isUp)
+            printf("\nCima");
+        if (isDown)
+            printf("\nBaixo");
+        if (isLeft)
+            printf("\nEsquerda");
+        if (isRight)
+            printf("\nDireita");
+        printf("\nMovimentos restantes: %d \n", quantidade - (i + 1));
     }
 }
 
@@ -137,17 +156,21 @@ void menuTorre()
 
 void movimentoTorre(char direcao, int quantidade)
 {
-    bool isLeft = direcao == 'A' ;
-    bool isRight = direcao == 'B' ;
-    bool isUp = direcao == 'C' ;
-    bool isDown = direcao == 'D' ;
+    bool isLeft = direcao == 'A';
+    bool isRight = direcao == 'B';
+    bool isUp = direcao == 'C';
+    bool isDown = direcao == 'D';
     for (int i = 0; i < quantidade; i++)
     {
-        if(isUp)printf("\nCima");
-        if(isDown)printf("\nBaixo");
-        if(isLeft)printf("\nEsquerda");
-        if(isRight)printf("\nDireita");
-        printf("\nMovimentos restantes: %d \n",quantidade - (i+1));
+        if (isUp)
+            printf("\nCima");
+        if (isDown)
+            printf("\nBaixo");
+        if (isLeft)
+            printf("\nEsquerda");
+        if (isRight)
+            printf("\nDireita");
+        printf("\nMovimentos restantes: %d \n", quantidade - (i + 1));
     }
 }
 
@@ -166,19 +189,107 @@ void menuRainha()
     printf("\n\nEscolha:");
 }
 
-
 void movimentoRainha(char direcao, int quantidade)
 {
     bool isLeft = direcao == 'A' || direcao == 'D' || direcao == 'E';
-    bool isRight = direcao == 'B' || direcao == 'C'  || direcao == 'F';
-    bool isUp = direcao == 'A' || direcao == 'B'  || direcao == 'G';
-    bool isDown = direcao == 'C' || direcao == 'D'  || direcao == 'H';
+    bool isRight = direcao == 'B' || direcao == 'C' || direcao == 'F';
+    bool isUp = direcao == 'A' || direcao == 'B' || direcao == 'G';
+    bool isDown = direcao == 'C' || direcao == 'D' || direcao == 'H';
     for (int i = 0; i < quantidade; i++)
     {
-        if(isUp)printf("\nCima");
-        if(isDown)printf("\nBaixo");
-        if(isLeft)printf("\nEsquerda");
-        if(isRight)printf("\nDireita");
-        printf("\nMovimentos restantes: %d \n",quantidade - (i+1));
+        if (isUp)
+            printf("\nCima");
+        if (isDown)
+            printf("\nBaixo");
+        if (isLeft)
+            printf("\nEsquerda");
+        if (isRight)
+            printf("\nDireita");
+        printf("\nMovimentos restantes: %d \n", quantidade - (i + 1));
+    }
+}
+
+void menuCavalo()
+{
+    printf("\n**** Cavalo Selecionado *****\n\n");
+    printf("Selecione a direção de movimento da peça: (Direcao inicial e posição final)");
+    printf("\n[A] Cima Esquerda");
+    printf("\n[B] Cima Direita");
+    printf("\n[C] Direita Cima");
+    printf("\n[D] Direita Baixo");
+    printf("\n[E] Baixo Direita");
+    printf("\n[F] Baixo Esquerda");
+    printf("\n[G] Esquerda Baixo");
+    printf("\n[H] Esquerda Cima");
+    printf("\n\nEscolha:");
+}
+
+void movimentoCavalo(char direcao)
+{
+    int movimentosCima = 0;
+    int movimentosBaixo = 0;
+    int movimentosDireita = 0;
+    int movimentosEsquerda = 0;
+
+    switch (direcao)
+    {
+    case 'A':
+        movimentosCima = 2;
+        movimentosEsquerda = 1;
+        break;
+    case 'B':
+        movimentosCima = 2;
+        movimentosDireita = 1;
+        break;
+    case 'C':
+        movimentosDireita = 2;
+        movimentosCima = 1;
+        break;
+    case 'D':
+        movimentosDireita = 2;
+        movimentosBaixo = 1;
+        break;
+    case 'E':
+        movimentosBaixo = 2;
+        movimentosDireita = 1;
+        break;
+    case 'F':
+        movimentosBaixo = 2;
+        movimentosEsquerda = 1;
+        break;
+    case 'G':
+        movimentosEsquerda = 2;
+        movimentosBaixo = 1;
+        break;
+    case 'H':
+        movimentosEsquerda = 2;
+        movimentosCima = 1;
+        break;
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (movimentosCima > 1)
+            printf("\nCima");
+        if (movimentosBaixo > 1)
+            printf("\nBaixo");
+        if (movimentosDireita > 1)
+            printf("\nDireita");
+        if (movimentosEsquerda > 1)
+            printf("\nEsquerda");
+        if (i == 1)
+        {
+            for (int c = 0; c < 1; c++)
+            {
+                if (movimentosCima == 1)
+                    printf("\nCima");
+                if (movimentosBaixo == 1)
+                    printf("\nBaixo");
+                if (movimentosDireita == 1)
+                    printf("\nDireita");
+                if (movimentosEsquerda == 1)
+                    printf("\nEsquerda");
+            }
+        }
     }
 }
